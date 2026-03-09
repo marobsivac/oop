703ed4dc089aaf99f40c3024d6f2aedfedbac342
Martin Obšivač <tvoje-github-email@nco.com>
2026-03-09 17:13:22 +0100

    Uprava project01

--- projects/project01/main.cpp
+++ projects/project01/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 
+
+class Trida {
+    private:
+        std::string email;
+        std::string osoba;
+        std::string dokument;
+
+
+    public:
+        Trida(std::string email, std::string osoba, std::string dokument) {
+            this->email = email;
+            this->osoba = osoba;
+            this->dokument = dokument;
+        }
+
+        void getter() {
+            std::cout << "Email: " << email << std::endl;
+            std::cout << "Osoba: " << osoba << std::endl;
+            std::cout << "Dokument: " << dokument << std::endl;
+        }
+};
+
+
 int main() {
-    std::cout << "Projekt 1\n";
+
+    Trida* objekt = new Trida("example@email.com", "John Doe", "Document 1");
+    objekt->getter();
+
+
+
+    return 0;
+    
 }
